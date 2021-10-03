066b052cba0076a7ffa590cc082239408ff7055e
Derek Banister <ptownderek@yahoo.com>
2021-10-02 21:02:34 -0700

    Added background image/minor css and beginning adding weather icons to forecast display

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,17 @@
+//CHECK GIVEN a weather dashboard with form inputs    
+//CHECK WHEN I search for a city 
+//CHECK THEN I am presented with current and future conditions for that city and that city is added to the search history
+//CHECK WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+//CHECK WHEN I view future weather conditions for that city
+//CHECK THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
 
 //declaring today date using moment.
-//moment().format("L");
+moment().format("L");
 
 //Steps:
 //On click event that handles the users city search. I need to make that input
@@ -58,6 +69,13 @@ function citySearch(cityname){
     console.log(response.main.humidity);
     var wind =  $("<p>").text("Wind Speed: " + response.wind.speed + " mph");
     console.log(response.wind.speed);
+    var weatherIcon = response.weather[0].main;
+    console.log(response.weather[0].main);
+        //if else? statements that display weather icons from openweathermap
+
+
+
+
 
     var weatherDisplay = $("<div>");
 
